ecdf2ced4705f822832b819b971d484e1fee0307
Dennux <Dennis@dennux.nl>
2026-07-22 17:28:07 +0200

    stap 4

--- nl.dennux.goodwexs/drivers/goodwe-xs/driver.js
+++ nl.dennux.goodwexs/drivers/goodwe-xs/driver.js
@@ -8,6 +8,17 @@ class GoodWeXSDriver extends Homey.Driver {
     this.log('GoodWe 2500-XS driver initialized');
   }
 
+  async onPairListDevices() {
+    return [
+      {
+        name: 'GoodWe 2500-XS',
+        data: {
+          id: 'goodwe-xs'
+        }
+      }
+    ];
+  }
+
 }
 
 module.exports = GoodWeXSDriver;
